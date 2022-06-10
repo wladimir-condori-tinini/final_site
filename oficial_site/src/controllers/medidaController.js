53b10c66f213734772ee7a463a2f49cb02f5f017
wladimir-condori-tinini <wladimir.tinini@sptech.school>
2022-06-10 09:43:08 -0300

    site finalizado

--- oficial_site/src/controllers/medidaController.js
+++ oficial_site/src/controllers/medidaController.js
@@ -1,14 +1,7 @@
 var medidaModel = require("../models/medidaModel");
 
-function buscarUltimasMedidas(req, res) {
-
-    const limite_linhas = 7;
-
-    var idAquario = req.params.idAquario;
-
-    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
-
-    medidaModel.buscarUltimasMedidas(idAquario, limite_linhas).then(function (resultado) {
+function buscarPostagensPorCategoria(req, res) {
+    medidaModel.buscarPostagensPorCategoria().then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -42,7 +35,7 @@ function buscarMedidasEmTempoReal(req, res) {
 }
 
 module.exports = {
-    buscarUltimasMedidas,
+    buscarPostagensPorCategoria,
     buscarMedidasEmTempoReal
 
 }
--- oficial_site/src/models/medidaModel.js
+++ oficial_site/src/models/medidaModel.js
@@ -1,14 +1,7 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-    instrucaoSql = `select 
-                        temperatura, 
-                        umidade, 
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+function buscarPostagensPorCategoria() {
+    instrucaoSql = `SELECT COUNT(id_postagem) AS posts, categoria_postagem FROM postagem GROUP BY categoria_postagem`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
@@ -27,6 +20,6 @@ function buscarMedidasEmTempoReal(idAquario) {
 
 
 module.exports = {
-    buscarUltimasMedidas,
+    buscarPostagensPorCategoria,
     buscarMedidasEmTempoReal
 }
